68dcdee640ede88d9d1c7aee951ccbe4ca807949
Torben <mail@sauce-code.com>
2018-05-18 00:31:22 +0200

    change end alert to type information

--- fittslaw/src/main/java/de/hska/iiwi/fittslaw/alerts/EndAlert.java
+++ fittslaw/src/main/java/de/hska/iiwi/fittslaw/alerts/EndAlert.java
@@ -11,10 +11,10 @@ public class EndAlert extends Alert {
 	private static final ObservableResourcesSingleton OBSERVABLE_RESOURCES = ObservableResourcesSingleton.getInstance();
 
 	public EndAlert() {
-		super(AlertType.CONFIRMATION);
+		super(AlertType.INFORMATION);
 		Stage stage = (Stage) getDialogPane().getScene().getWindow();
 		stage.getIcons().add(new Image(Constants.LOGO));
-		setHeaderText(OBSERVABLE_RESOURCES.getStringBinding("restartquestion").get());
+		setHeaderText(OBSERVABLE_RESOURCES.getStringBinding("end").get());
 //		setContentText(OBSERVABLE_RESOURCES.getStringBinding("restartquestion").get());
 	}
 

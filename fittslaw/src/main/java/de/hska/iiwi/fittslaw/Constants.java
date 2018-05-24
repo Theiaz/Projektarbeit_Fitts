6e34354084e287dc48e407c921eb5a7140924d2e
Torben <mail@sauce-code.com>
2018-05-25 00:02:37 +0200

    add some TODOs to Constants

--- fittslaw/src/main/java/de/hska/iiwi/fittslaw/Constants.java
+++ fittslaw/src/main/java/de/hska/iiwi/fittslaw/Constants.java
@@ -41,13 +41,13 @@ public class Constants {
 	public static final int DICT_LNGTH = 30;
 	
 	public static final int DEVICETYPE_LENGTH_MIN = 0;
-	public static final int DEVICETYPE_LENGTH_MAX = 100;
+	public static final int DEVICETYPE_LENGTH_MAX = 100; // TODO find good value
 	public static final int NAME_LENGTH_MIN = 1;
-	public static final int NAME_LENGTH_MAX = 100;
+	public static final int NAME_LENGTH_MAX = 100; // TODO find good value
 	public static final int AGE_MIN = 0;
 	public static final int AGE_MAX = 99;
 	public static final int COMMENT_LENGTH_MIN = 0;
-	public static final int COMMENT_LENGTH_MAX = 1000;
+	public static final int COMMENT_LENGTH_MAX = 1000; // TODO find good value
 	public static final int ROUNDS_MIN = 5; // TODO find good value
 	public static final int ROUNDS_MAX = 100; // TODO find good value
 
